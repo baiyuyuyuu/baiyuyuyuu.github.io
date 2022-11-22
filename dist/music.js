d405cd96880f720948a0e1f7dd492409195c0800
baiyuyuyuu <1244064459@qq.com>
2022-11-22 14:58:47 +0800

    Site updated: 2022-11-22 14:58:45

--- dist/music.js
+++ dist/music.js
@@ -4,7 +4,7 @@ const ap = new APlayer({
     loop: 'all',
     listMaxHeight: 50,
     theme: '#FADFA3', //主题色
-    fixed: true,
+    //fixed: true,
     order: 'list', //音频循环顺序, 可选值: 'list'列表循环, 'random'随机循环
     preload: 'auto', //预加载，可选值: 'none', 'metadata', 'auto'
     volume: 0.2, //默认音量，请注意播放器会记忆用户设置，用户手动设置音量后默认音量即失效
